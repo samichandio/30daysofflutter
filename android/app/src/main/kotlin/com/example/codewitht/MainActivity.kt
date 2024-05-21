3669222d481477c9cdc63d90dcf3df92923ab7bd
samichandio <chandiosami47@gmail.com>
2024-05-21 23:56:32 +0500

    fisrt commit

---
+++ android/app/src/main/kotlin/com/example/codewitht/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.codewitht
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Material(
+      child: Center(
+        child: Container(
+          child: const Text(" welcome to 30 days of flutter"),
+        ),
+      ),
+    ),
+  ));
+}
